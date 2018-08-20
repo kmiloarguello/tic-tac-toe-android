f92809a2e13d78290010215a467ada59831bd7c3
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-08-19 19:58:45 -0500

    comments before winning logic

--- app/src/main/java/xyz/camiloarguello/tictactoe/MainActivity.java
+++ app/src/main/java/xyz/camiloarguello/tictactoe/MainActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     int turn;
+    boolean[] boxes = {false,false,false,false,false,false,false,false,false};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +25,33 @@ public class MainActivity extends AppCompatActivity {
     public void dropInChip(View view){
         ImageView counter = (ImageView) view;
 
-        if(turn % 2 == 0){
-            counter.setImageResource(R.drawable.red);
-        }else{
-            counter.setImageResource(R.drawable.yellow);
+        // Getting item by tag
+        int tappedBox = Integer.parseInt(counter.getTag().toString());
+
+        // If the box tapped has not tapped before
+        if(!boxes[tappedBox]){
+
+            // Set the tapped box to true to prevent being tapped again
+            boxes[tappedBox] = true;
+
+            // If it's pair number puts the reds. Otherwise puts yellows
+            if(turn % 2 == 0){
+                counter.setImageResource(R.drawable.red);
+            }else{
+                counter.setImageResource(R.drawable.yellow);
+            }
+
+            counter.setScaleX(0f);
+            counter.setScaleY(0f);
+            counter.animate()
+                    .scaleX(1f)
+                    .scaleY(1f)
+                    .rotation(360f)
+                    .setDuration(500);
+            turn++;
+
         }
 
-        counter.setScaleX(0f);
-        counter.setScaleY(0f);
-        counter.animate()
-                .scaleX(1f)
-                .scaleY(1f)
-                .rotation(360f)
-                .setDuration(500);
-        turn++;
     }
 
 }
